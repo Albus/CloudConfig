db4149f52fe5c8aa549628c9240232619e87a922
Anton Rastyazhenko <rastyazhenko.anton@gmail.com>
2021-09-04 19:36:46 +0300

    commit

--- src/CloudConfig/lib/types.py
+++ src/CloudConfig/lib/types.py
@@ -15,13 +15,19 @@ class FS(str, Enum):
   ext4: str = "ext4"
 
 
+class UpStr(str):
+
+  def __new__(cls, s) -> object:
+    return super().__new__(cls, s.upper())
+
+
 class Disk:
   class Label(BaseModel):
     __root__: constr(regex="^LABEL=RANCHER_[A-Z0-9]+$", to_lower=False, strict=True, strip_whitespace=True)
 
     @validator("__root__", pre=True)
-    def validator_root(cls, v: str):
-      return v.upper()
+    def validator_root(cls, v: str) -> UpStr:
+      return UpStr(v)
 
   class Device(BaseModel):
     __root__: constr(regex="^/dev/[a-z0-9]+$", to_lower=True, strict=True, strip_whitespace=True)
